ebfb24b8b04a34d6ab805acfc6f65a5a0041b165
kazuki <kaz_cl_sys@icloud.com>
2021-03-25 16:18:50 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,66 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { useState, useReducer } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers/index";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const onChangeTitle = (e) => setTitle(e.target.value);
+  const onChangeBody = (e) => setBody(e.target.value);
+
+  const addEvent = (e) => {
+    e.preventDefault();
+    // dispatch
+    dispatch({ type: "CREATE_EVENT", title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  console.log(state);
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" />
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={onChangeTitle}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={onChangeBody}
+          />
         </div>
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>
+          イベントを作成する
+        </button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
         <thead>
-        <tr>
-          <th>ID</th>
-          <th>タイトル</th>
-          <th>ボディー</th>
-          <th></th>
-        </tr>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </tr>
         </thead>
         <tbody></tbody>
       </table>
     </div>
   );
-}
+};
 
 export default App;
